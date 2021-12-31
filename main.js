ce30eab8a642ae810d14084007642dc39939473c
Artem Legotin <arlegotin@gmail.com>
2021-12-31 09:02:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
 import VerstkaSDK from './src/sdk'
 
-const sdk = new VerstkaSDK({
-  apiKey: 'f87e056d977143ce4afdb07612d9cebb',
-  imagesOrigin: 'https://dvnw.ru',
-})
-
-const session = await sdk.openEditor({
-  userId: 1,
-  materialId: 1,
-})
-
-session.on('saved', data => {
-  console.log('saved', data)
-})
-
-// export default VerstkaSDK
+export default VerstkaSDK
